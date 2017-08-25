0a7d2aae894db6c6c6bbaec164b7ebc196c6b2c9
白桦 <afunx.bai@ubtrobot.com>
2017-08-25 12:31:54 +0800

    BubbleSort.java is modified and annotation is added

--- src/com/afunx/algorithm/sort/BubbleSort.java
+++ src/com/afunx/algorithm/sort/BubbleSort.java
@@ -5,19 +5,45 @@ import java.util.Random;
 
 public class BubbleSort {
 
-	private static int[] bubbleSort(int[] array) {
+	/**
+	 * Bubble Sort(ascending order)
+	 * 冒泡排序（升序排列）
+	 *
+	 * @param array
+	 * 				array  waiting for sort
+	 *            	待排序数组
+	 */
+	private static void bubbleSort(int[] array) {
 		final int size = array.length;
-		for (int i = 1; i < size; i++) {
-			for (int j = 1; j < size + 1 - i; j++) {
+		// i为每次冒泡排序的最大边界，每次循环过后，array[i]都是剩余元素中最大的
+		for (int i = size - 1; i > 0; i--) {
+			// j为每次冒泡排序进行比较的两个元素中，索引较大元素大索引位置
+			for (int j = 1; j <= i; j++) {
 				if (array[j - 1] > array[j]) {
-					// swap
-					array[j - 1] = array[j - 1] ^ array[j];
-					array[j] = array[j - 1] ^ array[j];
-					array[j - 1] = array[j - 1] ^ array[j];
+					swap(array, j - 1, j);
 				}
 			}
 		}
-		return array;
+	}
+
+	/**
+	 * Swap two elements in the array.
+	 * 交换数组中两个元素的位置。
+	 *
+	 * @param array
+	 * 				array waiting for swap
+	 * 				待交换数组
+	 * @param i
+	 * 				element position
+	 * 				元素位置
+	 * @param j
+	 * 				element position
+	 * 				元素位置
+	 */
+	private static void swap(int[] array, int i, int j) {
+		int temp = array[i];
+		array[i] = array[j];
+		array[j] = temp;
 	}
 
 	public static void main(String args[]) {
@@ -26,7 +52,7 @@ public class BubbleSort {
 		for (int i = 0; i < array.length; i++) {
 			array[i] = random.nextInt(20);
 		}
-		array = bubbleSort(array);
+		bubbleSort(array);
 		System.out.println(Arrays.toString(array));
 	}
 
