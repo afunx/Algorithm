b17db284489bf4dd4eedbb104baeb0d85536843b
afunx <afun1989@163.com>
2019-09-15 11:37:21 +0800

    optimize RadixSort.java

--- src/com/afunx/algorithm/sort/RadixSort.java
+++ src/com/afunx/algorithm/sort/RadixSort.java
@@ -1,55 +1,55 @@
 package com.afunx.algorithm.sort;
 
 import java.util.Arrays;
-import java.util.Random;
+
+import com.afunx.algorithm.util.ArrayUtils;
 
 public class RadixSort {
+
 	public static void main(String args[]) {
-		int[] array = new int[50];
-		Random random = new Random();
-		for (int i = 0; i < array.length; i++) {
-			array[i] = random.nextInt(20);
-		}
-		radixSort(array);
-		System.out.println(Arrays.toString(array));
+		final int size = 30;
+		final int count = 20;
+		int[] sourceArray = ArrayUtils.generateRandomArray(size, count);
+		int[] copyArray = Arrays.copyOf(sourceArray, sourceArray.length);
+		ArrayUtils.printArray(sourceArray);
+		radixSort(sourceArray);
+		ArrayUtils.printArray(sourceArray);
+		System.out.println(ArrayUtils.isAsendingOrder(sourceArray) && ArrayUtils.isArrayContentEqual(sourceArray, copyArray));
 	}
 
 	private static void radixSort(int[] array) {
-		final int radix = 10;
-		int max = array[0];
-		int length = array.length;
-		// find max value
-		for (int i = 1; i < length; i++) {
-			if (array[i] > max) {
-				max = array[i];
-			}
-		}
-		// find digits
-		int d = 0;
-		while (max != 0) {
-			++d;
-			max /= radix;
-		}
-
-		int[][] temp = new int[radix][length];
-		int[] order = new int[radix];
-		int n = 1;
+		// system(10 means decimal system)
+		final int d = 10;
+		boolean exit;
 		int k;
-		for (int m = 0; m < d; m++) {
-			for (int i = 0; i < array.length; i++) {
-				int reminder = (array[i] / n) % radix;
-				temp[reminder][order[reminder]++] = array[i];
-			}
-			k = 0;
-			for (int i = 0; i < radix; i++) {
-				if (order[i] != 0) {
-					for (int j = 0; j < order[i]; j++) {
-						array[k++] = temp[i][j];
-					}
-					order[i] = 0;
+		final int[][] tempArray = new int[d][array.length];
+		final int[] countArray = new int[d];
+		int n = 1;
+
+		do {
+			// whether exit the while loop
+			exit = true;
+			// find bucket and place array into tempArray
+			for(int i=0; i<array.length; i++) {
+				int bucket = array[i] / n % d;
+				if(exit && bucket > 0) {
+					exit = false;
 				}
+				tempArray[bucket][countArray[bucket]++] = array[i];
 			}
-			n *= radix;
-		}
+
+			// index for array
+			k = 0;
+			// place array from tempArray
+			for(int i=0; i<countArray.length; i++) {
+				for(int j=0; j<countArray[i]; j++) {
+					array[k++] = tempArray[i][j];
+				}
+				// clear countArray[i]
+				countArray[i] = 0;
+			}
+			// update n for calculate bucket
+			n *= d;
+		} while(!exit);
 	}
 }
